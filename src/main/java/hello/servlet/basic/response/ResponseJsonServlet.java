79db11965985bebf55023f0e7a24d25e7a2c0d62
kwYoohae <gocksdl7979@gmail.com>
2023-02-08 16:22:53 +0900

    [서블릿] HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "responseJsonservlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+  private ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  protected void service(final HttpServletRequest req, final HttpServletResponse response)
+      throws ServletException, IOException {
+    //Content-Type: application/json
+    response.setContentType("application/json");
+    response.setCharacterEncoding("utf-8");
+
+    HelloData helloData = new HelloData();
+    helloData.setUsername("kim");
+    helloData.setAge(20);
+
+    String result = objectMapper.writeValueAsString(helloData);
+    response.getWriter().write(result);
+  }
+}
