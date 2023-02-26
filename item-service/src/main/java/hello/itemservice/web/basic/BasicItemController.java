5e0fe664485229d93c8913aa1feb4c75084f5967
kwYoohae <gocksdl7979@gmail.com>
2023-02-26 13:52:16 +0900

    [스프링 MVC - 웹 페이지 만들기] 상품 수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -38,14 +40,67 @@ public class BasicItemController {
     return "/basic/addForm";
   }
 
+//  @PostMapping("/add")
+//  public String addItemV1(@RequestParam String itemName,
+//      @RequestParam int price,
+//      @RequestParam Integer quantity,
+//      Model model
+//  ) {
+//    Item item = new Item();
+//    item.setItemName(itemName);
+//    item.setPrice(price);
+//    item.setQuantity(quantity);
+//
+//    itemRepository.save(item);
+//
+//    model.addAttribute("item", item);
+//
+//    return "/basic/item";
+//  }
+
+//  @PostMapping("/add")
+//  public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+//    itemRepository.save(item);
+//
+////    model.addAttribute("item", item); 안해도 자동 등록
+//
+//    return "/basic/item";
+//  }
+
+//  @PostMapping("/add")
+//  public String addItemV3(@ModelAttribute Item item) {
+//    itemRepository.save(item);
+//
+//    // Item 이므로 item 으로 자동등록, 첫번 째만 소문자로 바뀜
+//
+//    return "/basic/item";
+//  }
+
   @PostMapping("/add")
-  public String save() {
-    return "xxx";
+  public String addItemV4(Item item) {
+    itemRepository.save(item);
+
+    // Item 이므로 item 으로 자동등록, 첫번 째만 소문자로 바뀜
+
+    return "/basic/item";
+  }
+
+  @GetMapping("/{itemId}/edit")
+  public String editForm(@PathVariable Long itemId, Model model) {
+    final Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/editForm";
+  }
+
+  @PostMapping("/{itemId}/edit")
+  public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    itemRepository.update(itemId, item);
+    return "redirect:/basic/items/{itemId}";
   }
 
   /*
-  * 테스트용 데이터 추가
-  * */
+   * 테스트용 데이터 추가
+   * */
   @PostConstruct
   public void init() {
     itemRepository.save(new Item("itemA", 100000, 10));
